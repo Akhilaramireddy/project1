9c3b46cdffce5b9bad17691787d59628082420e2
Akhilaramireddy <akhilaramireddy67@gmail.com>
2023-05-20 13:05:06 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def surjana(request):
+    return HttpResponse('<marquee><h1>surjana thinnavara......</marquee></h1>')
+
+
+def revi(request):
+    return HttpResponse('long drive podhama')
+
+
+def akhi(request):
+    return 
